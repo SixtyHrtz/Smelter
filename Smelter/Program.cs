26cdde542cebf612efd1be17994d07d27d544a63
SixtyHz <sixtyhz@outlook.com>
2018-08-14 09:43:56 +0300

    Инициализация репозитория

---
+++ Smelter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
